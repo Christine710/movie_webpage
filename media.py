078cc67812ab0aef96d259220f5c4480d5c783fb
Christine710 <christine.oeveren@gmail.com>
2017-09-29 12:02:49 +0200

    added docstrings to the media file

--- media.py
+++ media.py
@@ -2,15 +2,23 @@ import webbrowser
 
 class Movie():
     """ This class provides a way to store movie related information"""
-
-    valid_ratings = ["G", "PG", "PG-13", "R"]
-
     def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
-        # type: (object, object, object, object) -> object
+        """
+        This definition makes all the variables in the class movie.
+
+        :param movie_title:     (String)
+        :param movie_storyline: (String)
+        :param poster_image:    (String - webpage)
+        :param trailer_youtube: (String - webpage)
+        """
+
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
 
     def show_trailer(self):
+        """
+        This method uses the webbrowser library to open the youtube trailer
+        """
         webbrowser.open(self.trailer_youtube_url)
